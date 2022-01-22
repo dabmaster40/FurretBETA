337f21017d5e6081d16e10f3a183387179512323
Leo Mendoza <82682230+dabmaster40@users.noreply.github.com>
2022-01-22 15:06:23 -0500

    Update stats.py

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from discord import Embed
 from discord.ext.commands import Bot, Cog
 from discord_slash import cog_ext, SlashContext
@@ -10,7 +11,32 @@ class embed(Cog):
 
     @cog_ext.cog_slash(name="Stats", description="Stats for Furret aka the nerd command")
     async def stats(self, ctx: SlashContext):
-        embed = Embed(title="Bot Stats", description="Created by dabmaster40#6556 with help from Dauhxe#1489")
+        delta_uptime = datetime.utcnow() - self.bot.launch_time
+        hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days, hours = divmod(hours, 24)
+
+        embed = Embed(color=0x7289da)
+        embed.set_author(
+        name="Furret Stats")
+        embed.add_field(
+        name="Python Version:",
+        value=f"3.8",
+        inline=True)
+        embed.add_field(
+        name="Ping:",
+        value= f"{round(self.bot.latency * 1000)}ms",
+        inline=True)
+        embed.add_field(
+        name="Uptime:",
+        value= f"{days}d, {hours}h, {minutes}m",
+        inline=True)
+        embed.add_field(
+        name="Servers:",
+        value= f"{len(self.bot.guilds)}",
+        inline=True)
+        embed.set_footer(
+        text=f"Created by dabmaster40#6556 and Dauhxe#1489")
         await ctx.send(embed=embed)
 
 def setup(bot: Bot):
